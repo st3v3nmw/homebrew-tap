2bc94a4880ed9b54eee8f594dbb4783bca71ec55
Stephen Mwangi <mail@stephenmwangi.com>
2024-12-24 14:22:52 +0300

    update sidekick to v0.1.1

--- Formula/sidekick.rb
+++ Formula/sidekick.rb
@@ -1,7 +1,7 @@
 class Sidekick < Formula
     desc "A CLI tool that understands your natural language commands and executes them for you"
     homepage "https://github.com/st3v3nmw/sidekick"
-    url "https://github.com/st3v3nmw/sidekick.git", tag: "v0.1.0"
+    url "https://github.com/st3v3nmw/sidekick.git", tag: "v0.1.1"
 
     depends_on "go" => :build
 
