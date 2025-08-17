7e0868961ddf9501a2512a52966c1916f704b452
Stephen Mwangi <mail@stephenmwangi.com>
2025-08-17 20:32:02 +0300

    add homebrew formula for sourcerer

---
+++ Formula/sourcerer.rb
@@ -0,0 +1,14 @@
+class Sourcerer < Formula
+    desc "MCP for semantic code search & navigation that reduces token waste "
+    homepage "https://github.com/st3v3nmw/sourcerer-mcp"
+    url "https://github.com/st3v3nmw/sourcerer-mcp.git", tag: "v0.1.0"
+
+    depends_on "go" => :build
+
+    def install
+      system "go", "mod", "download"
+      system "go", "build", "-o", "sourcerer", "./cmd/sourcerer"
+
+      bin.install "sourcerer"
+    end
+  end
